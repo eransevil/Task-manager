93d57ac896b53ee58d29d029db13380a3e6f1300
your_name <your_address_email@domain.com>
2020-05-03 19:19:08 +0300

    sort

--- src/index.js
+++ src/index.js
@@ -19,13 +19,13 @@ app.listen(port, () =>{
 const Task = require('./models/task')
 const User = require('./models/user')
 
-const main = async() =>{
-    const task = await Task.findById('5eada2be919f996580bee0da')
-    await task.populate('owner').execPopulate()
-    console.log(task.owner)
+// const main = async() =>{
+//     const task = await Task.findById('5eada2be919f996580bee0da')
+//     await task.populate('owner').execPopulate()
+//     console.log(task.owner)
 
-    const user = await User.findById('5ead86624e712d6738f02b09')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-main()
+//     const user = await User.findById('5ead86624e712d6738f02b09')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
+// main()
